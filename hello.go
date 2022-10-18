0bb1fb93c66655762a99a5379d10275986b1863a
Chunghwan <chunghwan@syllabs.io>
2022-10-18 15:58:48 +0900

    Initialize Project

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("Chris", "Spanish"))
+}
